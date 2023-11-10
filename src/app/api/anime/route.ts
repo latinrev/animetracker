11a95fbb27f90fe50942f341c51e83f47e7b43ff
latinrev <latinreve@gmail.com>
2023-11-09 22:25:01 -0600

    feat(auth): remove next auth

--- src/app/api/anime/route.ts
+++ src/app/api/anime/route.ts
@@ -3,12 +3,10 @@ import { NextResponse } from "next/server";
 import bcrypt from "bcrypt"
 import { createUser } from "@/services/userDb";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/utils/auth";
 import { fetchAnimes } from "@/services/animeDb";
 
 
 export async function GET(req: Request) {
-    const session = await getServerSession(authOptions)
     const body = await req.json()
     const { id } = body
 
