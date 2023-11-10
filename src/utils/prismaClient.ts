f6e18502fe18719059e3000ad73b5f811e1ae973
latinrev <latinreve@gmail.com>
2023-11-09 22:22:08 -0600

    feat(auth): remove next auth

--- src/utils/prismaClient.ts
+++ src/utils/prismaClient.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 import { getServerSession } from 'next-auth';
-// import { authOptions } from './auth';
 
 
 const DBClient = {
@@ -12,7 +11,6 @@ Object.freeze(DBClient);
 const client = DBClient.instance
 
 // client.$use(async (params, next) => {
-//     const session = await getServerSession(authOptions) as any
 //     if (session && params.args.where && params.model === "anime") {
 //         if (params.action === "findFirst" || params.action === "findMany") {
 //             params.args.where.AND.userId = session?.user?.sub
@@ -22,7 +20,6 @@ const client = DBClient.instance
 //     return result
 // })
 // client.$use(async (params, next) => {
-//     const session = await getServerSession(authOptions) as any
 //     if (session && params.model === "anime" && params.action === 'create') {
 //         params.args.data.userId = session?.user?.sub
 //     }
